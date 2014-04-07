74ecfbce82ab2c83adbdf7adb900a2432dc6de0d
Dylon Edwards <dylon.edwards@gmail.com>
2014-04-07 05:39:42 -0700

    Drops a deprecated reference

--- src/main/java/com/github/dylon/liblevenshtein/levenshtein/AbstractDistanceAndTermComparator.java
+++ src/main/java/com/github/dylon/liblevenshtein/levenshtein/AbstractDistanceAndTermComparator.java
@@ -3,23 +3,19 @@ package com.github.dylon.liblevenshtein.levenshtein;
 import java.util.Comparator;
 
 import lombok.AccessLevel;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import lombok.experimental.FieldDefaults;
 
 /**
  * @author Dylon Edwards
  * @since 2.1.0
  */
+@Accessors(fluent=true)
 @FieldDefaults(level=AccessLevel.PROTECTED)
 public abstract class AbstractDistanceAndTermComparator
   implements Comparator<Intersection> {
 
   /** Query term to consider while sorting correction candidates */
-  String term;
-  String lowerTerm;
-
-  public AbstractDistanceAndTermComparator term(final String term) {
-    this.term = term;
-    this.lowerTerm = term.toLowerCase();
-    return this;
-  }
+  @Setter String term;
 }
