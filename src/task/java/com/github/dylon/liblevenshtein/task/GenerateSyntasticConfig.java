6d8649fff5ab554dfdfa95d94a4cf1f8551a6f1f
Dylon Edwards <dylon.edwards@gmail.com>
2015-07-03 19:28:20 -0700

    Adds templating logic for the GitHug, wiki documentation and Syntastic configs

---
+++ src/task/java/com/github/dylon/liblevenshtein/task/GenerateSyntasticConfig.java
@@ -0,0 +1,29 @@
+package com.github.dylon.liblevenshtein.task;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupDir;
+import org.stringtemplate.v4.ST;
+
+public class GenerateSyntasticConfig {
+  public static void main(final String... args) throws IOException {
+    final String classpath = args[0];
+    final Path configPath = Paths.get(args[1]);
+
+    final STGroup group = new STGroupDir("stringtemplate");
+		final ST template = group.getInstanceOf("syntastic");
+		template.add("classpath", classpath);
+
+		String configText = template.render();
+		if ('\n' != configText.charAt(configText.length() - 1)) {
+			configText += "\n"; // make sure it ends with a newline
+		}
+
+		Files.write(configPath, configText.getBytes(StandardCharsets.UTF_8));
+	}
+}
---
+++ src/task/java/com/github/dylon/liblevenshtein/task/GenerateWikidoc.java
@@ -0,0 +1,46 @@
+package com.github.dylon.liblevenshtein.task;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupDir;
+import org.stringtemplate.v4.ST;
+
+public class GenerateWikidoc {
+	public static void main(final String... args) throws IOException	{
+		int argsIdx = 0;
+
+		final String groupId = args[argsIdx ++];
+		final String artifactId = args[argsIdx ++];
+		final String version = args[argsIdx ++];
+
+		final Path wikidocDir = Paths.get(args[argsIdx ++]);
+
+		final STGroup group = new STGroupDir("stringtemplate/wiki/", '$', '$');
+
+		final String[] templateNames = {
+			"installation",
+			"building",
+			"testing",
+			"usage"
+		};
+
+		System.out.println();
+		for (final String templateName : templateNames) {
+			final ST template = group.getInstanceOf(templateName);
+			template.add("groupId", groupId);
+			template.add("artifactId", artifactId);
+			template.add("version", version);
+
+			final String wikidoc = template.render() + "\n";
+			final Path wikidocPath = wikidocDir.resolve(templateName + ".java.md");
+			System.out.printf("Writing rendered template [%s] to [%s]\n",
+					templateName, wikidocPath);
+			Files.write(wikidocPath, wikidoc.getBytes(StandardCharsets.UTF_8));
+		}
+	}
+}
